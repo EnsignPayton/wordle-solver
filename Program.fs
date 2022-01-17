217237a8478706761a7ab078e9495c2c4a438dc4
Logan Payton <Logan.Payton@javs.com>
2022-01-17 00:03:59 -0500

    Make it work

--- Program.fs
+++ Program.fs
@@ -1,4 +1,61 @@
-﻿
+﻿// All possible words
+let readLines filePath = System.IO.File.ReadLines(filePath)
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let isLength len (word:string) = word.Length = len
+let ofLength len words:seq<string> =
+    words
+    |> Seq.filter (isLength len)
+
+// Exclude grey letters
+let intersect a b = Set.intersect (Set.ofList a) (Set.ofList b)
+let hasIntersection a b = intersect a b |> Set.isEmpty
+
+let hasLetters (letters:list<char>) (word:string) =
+    Seq.toList word
+    |> hasIntersection letters
+let excludingLetters (letters:list<char>) (words:seq<string>) =
+    words
+    |> Seq.filter (hasLetters letters)
+
+// Filter by green letters
+type LetterPos = {Letter:char; Position:int}
+
+let hasLetterPos (word:string) (lp:LetterPos) = word[lp.Position] = lp.Letter
+let hasLetterPoses (lps:list<LetterPos>) (word:string) =
+    lps
+    |> List.forall (hasLetterPos word)
+let withCorrectLetters (lps:list<LetterPos>) (words:seq<string>) =
+    words
+    |> Seq.filter (hasLetterPoses lps)
+
+// Filter by yellow letters
+let hasLetter letter word =
+    Seq.toList word
+    |> List.contains letter
+let hasLetterNoPos (word:string) (lp:LetterPos) =
+    (hasLetter lp.Letter word) && not (hasLetterPos word lp)
+let hasLetterNoPoses (lps:list<LetterPos>) (word:string) =
+    lps
+    |> List.forall (hasLetterNoPos word)
+let withMisplacedLetters (lps:list<LetterPos>) (words:seq<string>) =
+    words
+    |> Seq.filter (hasLetterNoPoses lps)
+
+// Main
+let length = 5
+let excluded = ['r';'s';'t';'l';'n';'e';'z';'x';'w']
+let correct = [
+    { Letter = 'b'; Position = 0 };
+    { Letter = 'y'; Position = 4 };
+]
+let misplaced = [
+    { Letter = 'u'; Position = 3 }
+]
+let words =
+    readLines "words_alpha.txt"
+    |> ofLength length
+    |> excludingLetters excluded
+    |> withCorrectLetters correct
+    |> withMisplacedLetters misplaced
+
+words |> Seq.iter(fun x -> printfn "%s" x)
---
+++ README.md
@@ -0,0 +1,7 @@
+# Wordle Solver
+
+Remind myself of F# syntax with a tool for the internet's latest word game. This is probably terrible code, I haven't used F# in years.
+
+Word list from https://github.com/dwyl/english-words
+
+Wordle game here: https://www.powerlanguage.co.uk/wordle/
